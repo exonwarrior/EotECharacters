7f5438ece81a91954a5d3e8488e936af04cd62c6
Dave Haenze <drhaenze@gmail.com>
2019-10-30 22:30:58 +0100

    Example login and connection conf

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3036');
+   define('DB_USERNAME', 'eote');
+   define('DB_PASSWORD', 'C1oudbur5t');
+   define('DB_DATABASE', 'edge_of_the_empire');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
